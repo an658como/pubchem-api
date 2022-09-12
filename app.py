b406c7996ee3b339f05c46dfb800657c0cc5f3f6
Ali Naseri <alinaseri.ut@gmail.com>
2022-09-12 16:52:07 -0400

    dev-pubchem-api: the code is able to communicate with pubchem API. The user enters smiles and some information will be printed on the screen.

--- app.py
+++ app.py
@@ -3,26 +3,12 @@
 
 # import the Flask class from the flask moodule
 from flask import Flask, render_template
-# import the SQL_Alchemy for your data storage (from the module ?? import the class ??)
-from flask_sqlalchemy import SQLAlchemy
-
-
 
 
 # instanciate the Flask class. The object name is app by convention
 app = Flask(__name__) # the variable name is passed for initialization
 # secret key added here to avoid the csrf_token blockage
 app.config['SECRET_KEY'] = 'sample_key'
-# now that you are adding a database, the config file needs to be updated for it
-# for this simple example sqlite is being used for its simplicity
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-
-
-# create an object of your database for your app
-db = SQLAlchemy(app)
-# the database is created but the data model is missing. The data model should be defined
-# in a separte file
-
 
 
 # import all routes from the routes module 
--- models.py
+++ models.py
@@ -1,14 +1,11 @@
-# in this file, we want to define the data model. 
-# thus, the data object must be imported here
-from app import db
-
 
 # create a class which describes your data model
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+class Task():
+    def __init__(self, title):
+        self.id = None
+        self.title = title
+        self.date = None
     
     # method to represent the data
     def __repr__(self):
